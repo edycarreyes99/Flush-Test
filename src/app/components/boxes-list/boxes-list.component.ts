8625deeb19cf76bbdb2e81fd7372e0bdfdc924c3
Edycar Reyes <edycarreyes@gmail.com>
2024-10-11 16:53:16 -0600

    feature, added the other box items

--- src/app/components/boxes-list/boxes-list.component.ts
+++ src/app/components/boxes-list/boxes-list.component.ts
@@ -15,6 +15,41 @@ export class BoxesListComponent {
       image: '../../../assets/img/boxes/box-1.svg',
       price: 59.99,
       isNew: true
+    },
+    {
+      title: 'Treasure Trove',
+      description: 'Eerie Enigmas',
+      image: '../../../assets/img/boxes/box-2.svg',
+      price: 120.00,
+      isNew: true
+    },
+    {
+      title: 'Crypto Crate',
+      description: 'Eerie Enigmas',
+      image: '../../../assets/img/boxes/box-3.svg',
+      price: 88.99,
+      isNew: false
+    },
+    {
+      title: 'Concealed Curiosities',
+      description: 'Mystery Marvels',
+      image: '../../../assets/img/boxes/box-4.svg',
+      price: 99.99,
+      isNew: false
+    },
+    {
+      title: 'Mystic Vault',
+      description: 'Esoteric Earnings',
+      image: '../../../assets/img/boxes/box-5.svg',
+      price: 87.56,
+      isNew: false
+    },
+    {
+      title: 'Conundrum Cache',
+      description: 'Esoteric Earnings',
+      image: '../../../assets/img/boxes/box-6.svg',
+      price: 42.99,
+      isNew: false
     }
   ];
 }
