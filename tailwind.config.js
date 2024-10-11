8fd0de2cdca0d9cd00b1d3ddef551327717ca480
Edycar Reyes <edycarreyes@gmail.com>
2024-10-11 14:24:44 -0600

    feature, added the flush background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
         'flush-gradient': 'linear-gradient(358.1deg, rgba(95, 159, 255, 1), rgba(43, 43, 43, 1))',
       },
       backgroundColor:{
-        'flush-link': 'rgba(255, 255, 255, 0.1)'
+        'flush-link': 'rgba(255, 255, 255, 0.1)',
+        'flush-background': '#101315',
       },
       borderColor: {
         'flush-gray': 'rgba(255, 255, 255, 0.5)',
